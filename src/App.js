3af465cff44fe0e0be6ed7b27dd0cd85e7048340
rafsan <rafsanamin18@gmail.com>
2020-11-20 06:44:02 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ChooseTeam from "./Components/ChooseTeam/ChooseTeam";
+import Footer from "./Components/Footer/Footer/Footer";
+
+function App() {
+    return (
+        <div>
+            <ChooseTeam></ChooseTeam>
+        </div>
+    );
+}
+
+export default App;
